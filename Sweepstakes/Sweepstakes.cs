bef6e5b51f9dcaaa2a791b01061c7a311c70df5f
Christopher Couch <c.alan.couch@gmail.com>
2020-11-02 14:02:37 -0600

    Created member methods Sweepstakes(), RegisterContestant(), PickWinner(), and PrintContestantInfo() for the Sweepstakes class.

--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -10,7 +10,43 @@ namespace Sweepstakes
     {
         private Dictionary<int, Contestant> contestants;
         private string name;
-        public string Name;
+        Random random = new Random();
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        
+        }
+        public Sweepstakes(string nameOfSweepstakes)
+        {
+            name = nameOfSweepstakes;
+            contestants = new Dictionary<int, Contestant>();
+            
+        }
+        public void RegisterConstestant(Contestant contestant)
+        {
+
+            contestants.Add(contestant.RegistrationNumber, contestant);
+        }
+        public Contestant PickWinner()
+        {
+            List<int> keyList = new List<int>(contestants.Keys);
+            int randomConstestantKey = keyList[random.Next(keyList.Count)];
+            return contestants[randomConstestantKey];
+
+        }   
+        public void PrintContestantInfo(Contestant contestant)
+        {
+            Console.WriteLine($"This is {contestant.FirstName} {contestant.LastName}, their\n" +
+                $" email is {contestant.EmailAddress} with a Registration no. of {contestant.RegistrationNumber}.");
+
+        }
 
     }
 }
